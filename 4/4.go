55797baffa88b3f706d5a9fb5307a412d05a9dd4
Brian Chau <brian.chau@outlook.com>
2021-11-27 15:46:43 -0800

    Updated challenge 4

--- 4/4.go
+++ 4/4.go
@@ -8,41 +8,58 @@ import (
     "net/http"
     "regexp"
     "strconv"
+    "strings"
 )
+
+func read_html(url string, param string) (string) {
+    // Do an HTTP Get request
+    resp, err := http.Get(url + param)
+    if err != nil {
+        log.Fatalf("Could not open URL")
+    }
+    defer resp.Body.Close()
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatalf("Could not read body")
+    }
+    return string(body)
+}
+
+func check_special_conditions(body string, param string) (string, bool) {
+    // "peak.html" is the end condition
+    if body == "peak.html" {
+        fmt.Println("peak.html")
+        os.Exit(3)
+    } else if strings.Contains(body, "Divide by two") {
+        // Calculate the next step, if it says to divide by two
+        tmp, err := strconv.Atoi(param)
+        if err != nil {
+            log.Fatalf("Could not convert number to string, for some reason.")
+        }
+        tmp     = tmp / 2
+        param   = strconv.Itoa(tmp)
+        return param, true
+    }
+    return "", false
+}
 
 func main() {
     param := "12345"
-    url := "http://pythonchallenge.com/pc/def/linkedlist.php?nothing="
+    url   := "http://pythonchallenge.com/pc/def/linkedlist.php?nothing="
     for true {
         // Do an HTTP Get request
-        resp, err := http.Get(url + param)
-        if err != nil {
-            log.Fatalf("Could not open URL")
-        }
-        defer resp.Body.Close()
-        body, err := io.ReadAll(resp.Body)
-        if err != nil {
-            log.Fatalf("Could not read body")
-        }
-        res := string(body)
+        body := read_html(url, param)
 
         // Check for special conditions
-        if res == "peak.html" {
-            fmt.Println("peak.html")
-            os.Exit(3)
-        } else if res == "Yes. Divide by two and keep going." {
-            tmp, err := strconv.Atoi(param)
-            if err != nil {
-                log.Fatalf("Could not convert number to string, for some reason.")
-            }
-            tmp     = tmp / 2
-            param   = strconv.Itoa(tmp)
+        tmp, found := check_special_conditions(body, param)
+        if found {
+            param = tmp
             continue
         }
 
         // Otherwise, the standard condition is "and the next nothing is <some number>"
         next_pattern := regexp.MustCompile("and the next nothing is (\\d+)")
-        next_param   := next_pattern.FindAllStringSubmatch(res,-1)
+        next_param   := next_pattern.FindAllStringSubmatch(body,-1)
         for _, i := range next_param {
             fmt.Printf("%s\n", i[1])
         }
